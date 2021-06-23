2688f05abc364143b0dbb46d84c107f535663017
Memo <gdelvalle@nevada.unr.edu>
2021-06-23 08:46:27 -0700

    Finalizing STMapAuto

--- src/main/java/com/unrmedlab/imagej/BioLabPlugin2.java
+++ src/main/java/com/unrmedlab/imagej/BioLabPlugin2.java
@@ -6,9 +6,10 @@
  *     http://creativecommons.org/publicdomain/zero/1.0/
  */
 
-package com.mycompany.imagej;
+package com.unrmedlab.imagej;
 
 import trainableSegmentation.WekaSegmentation;
+import trainableSegmentation.Weka_Segmentation;
 import trainableSegmentation.utils.Utils;
 
 
@@ -17,17 +18,19 @@ import org.scijava.plugin.Plugin;
 import ij.IJ;
 import ij.ImagePlus;
 import ij.gui.GenericDialog;
+import ij.gui.NonBlockingGenericDialog;
 import ij.gui.Roi;
 import ij.io.OpenDialog;
+import ij.plugin.frame.Recorder;
 import ij.plugin.frame.RoiManager;
 import ij.process.ImageStatistics;
 
 
 import java.io.File;
 import java.io.FileNotFoundException;
-
+import java.nio.file.*;
 import java.io.IOException;
-
+import java.util.logging.Logger;
 
 import ij.plugin.filter.Analyzer;
 import ij.plugin.filter.ParticleAnalyzer;
@@ -49,7 +52,6 @@ public class BioLabPlugin2 implements Command {
 		public double height = .29;
 		public int depth = 0;
 		public String unit = "um";
-		
 
     @Override
     public void run() {
@@ -59,6 +61,9 @@ public class BioLabPlugin2 implements Command {
     	is = new ImageStatistics();
     	numberDialog();
     	ParticleAnalyzer.setRoiManager(rm);
+    	String choice = "";
+    	choice = choiceDialog(choice);
+    	if(choice == "Batch Processing") {
     	genDialog("Choose input folder");
     	String input_dir = IJ.getDirectory("Choose input folder");//this is where the images come from
     	if(input_dir == null) {
@@ -78,9 +83,42 @@ public class BioLabPlugin2 implements Command {
         
         final File folder = new File(input_dir);
         processFilesForFolder(folder, output_dir, path);
+    	}
+    	else {
+    		genDialog("Choose an image");
+    		OpenDialog dialog = new OpenDialog("Choose an image", null);
+    		genDialog("Choose output folder");
+        	String output_dir = IJ.getDirectory("Choose output folder");//this is where they are stored
+        	if(output_dir == null) {
+        		return;
+        	}
+        	String path = dialog.getPath();
+        	String file = dialog.getFileName();
+        	String dir = dialog.getDirectory();
+        	if(path == null) {
+        		return;
+        	}
+        	trainClassifier(path);
+        	//Weka_Segmentation segment = new Weka_Segmentation();
+        	nonDialog("Click okay when done");
+        	Weka_Segmentation.getResult();
+        	ImagePlus imp1 = IJ.getImage();
+        	imp = IJ.openImage(path);
+        	processSingleImage(imp1, output_dir);
+    	}
         
     }
-
+    
+    public String choiceDialog(String choice) {
+    	GenericDialog gd = new GenericDialog("Choose a mode");
+    	gd.addChoice("Pick mode of operation", new String[] {"Batch Processing", "Single Image Processing"}, "Batch Processing" );
+    	gd.showDialog();
+    	if(gd.wasCanceled()){
+    		return choice;
+    	}
+    	choice = gd.getNextChoice();
+    	return choice;
+    }
     
     public boolean properties() {
     	IJ.run(imp, "Properties...", "channels=1 slices=1 frames=1 unit=" + unit + " pixel_width="+width+" pixel_height="+height+" voxel_depth="+depth);
@@ -109,49 +147,64 @@ public class BioLabPlugin2 implements Command {
     	counter = 0;
     }
     
+    public void trainClassifier(String img_path) {
+    	IJ.run("Trainable Weka Segmentation", "open="+img_path);
+    }
     
-    public void processImage(String img, String output_folder, String classifier, int check) throws FileNotFoundException, IOException {
+    public void processSingleImage(ImagePlus imp1, String output_folder) {
+    	properties();
     	rm = new RoiManager(true);
     	ParticleAnalyzer.setRoiManager(rm);
     	pa = new ParticleAnalyzer(2048, 0, new ResultsTable(), min, max);
+    	Path path = Paths.get(output_folder + imp);
+    	if(!Files.exists(path)) {
+    		File dir = new File(output_folder + imp);
+        	dir.mkdir();
+    	}
+    	IJ.run("Enhance Contrast", "saturated=0.3");
+   		IJ.setAutoThreshold(imp1, "Intermodes");
+   		IJ.run("Set Measurements...", "area mean bounding fit limit redirect=None decimal=3");	
+   		pa.analyze(imp1);
+   		imp1.close();
+    	imp1 = null;
+    	Analyzer.setMeasurements(1 + 2048 + 256 + 2 + 512);
+    	a = new Analyzer(imp, rt);
+    	Roi[] rois = rm.getRoisAsArray();
+    	for(int i = 0; i<rois.length;i++) {
+    		imp.setRoi(rois[i]);
+    		IJ.run(imp, "Add Selection...", "");
+    		is = imp.getStatistics(1 + 2048 + 256 + 2 + 512);
+    		imp.saveRoi();
+    		a.saveResults(is, rois[i]);
+    	}
+    	ImagePlus imp2 = imp.flatten();
+    	IJ.saveAsTiff(imp2, output_folder + imp + "/Mask");
+    	rt.save(output_folder + imp + "/Results.xls");
+   		nullify();
+   		return;
+    }
+    
+    public void processImage(String img, String output_folder, String classifier, int check) throws FileNotFoundException, IOException {
         imp = IJ.openImage(img);
         if(imp != null) {
         	imp.show();
         	File dir = new File(output_folder + imp);
         	dir.mkdir();
-        	properties();	
+       		saveSegmentation(classifier, output_folder);
         	weka = new WekaSegmentation(imp);
         	if(weka.loadClassifier(classifier) == false) {
         		nullify();
        			return;
        		}
         
-       		saveSegmentation(classifier, output_folder);
+       		
        		weka.applyClassifier(false);
        		ImagePlus result = new ImagePlus();
        		result = weka.getClassifiedImage();
        		result.show();
-       		IJ.run("Enhance Contrast", "saturated=0.3");
-       		IJ.setAutoThreshold(result, "Intermodes");
-       		IJ.run("Set Measurements...", "area mean bounding fit limit redirect=None decimal=3");	
-       		pa.analyze(result);
-       		result.close();
-        	result = null;
-        	Analyzer.setMeasurements(1 + 2048 + 256 + 2 + 512);
-        	a = new Analyzer(imp, rt);
-        	Roi[] rois = rm.getRoisAsArray();
-        	for(int i = 0; i<rois.length;i++) {
-        		imp.setRoi(rois[i]);
-        		IJ.run(imp, "Add Selection...", "");
-        		is = imp.getStatistics(1 + 2048 + 256 + 2 + 512);
-        		imp.saveRoi();
-        		a.saveResults(is, rois[i]);
-        	}
-        	ImagePlus imp2 = imp.flatten();
-        	IJ.saveAsTiff(imp2, dir + "/Mask");
-        	rt.save(dir + "/Results.xls");
-       		nullify();
-       		return; 
+       		
+       		
+       		processSingleImage(result, output_folder);
 			
        }
     } 
@@ -210,6 +263,13 @@ public class BioLabPlugin2 implements Command {
    
     public void genDialog(String s) {
     	GenericDialog gd = new GenericDialog(s);
+    	gd.showDialog();
+    	if(gd.wasCanceled()) {
+    		return;
+    	}
+    }
+    public void nonDialog(String s) {
+    	NonBlockingGenericDialog gd = new NonBlockingGenericDialog(s);
     	gd.showDialog();
     	if(gd.wasCanceled()) {
     		return;
